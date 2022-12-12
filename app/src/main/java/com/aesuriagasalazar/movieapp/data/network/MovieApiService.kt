e3accc4996a20ad62bcb0762cfe9ef84ae764bd8
Angel Eduardo Suriaga Salazar <angelsuriaga@hotmail.com>
2022-12-12 15:59:05 -0500

    Initial commit

---
+++ app/src/main/java/com/aesuriagasalazar/movieapp/data/network/MovieApiService.kt
@@ -0,0 +1,9 @@
+package com.aesuriagasalazar.movieapp.data.network
+
+import retrofit2.Retrofit
+
+private const val BASE_URL = ""
+private val retrofit = Retrofit.Builder()
+
+class MovieApiService {
+}
